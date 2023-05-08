d5b714f46bfc1465fe897ee9ece1aafd852975b8
Mike <michael.daniel.hampton@gmail.com>
2023-05-08 20:33:33 +0200

    Some error handling for post

---
+++ src/routes/posts/[id]/+error.svelte
@@ -0,0 +1,4 @@
+<div class="error">
+    <h1>404</h1>
+    <h4>Something's Wrong</h4>
+</div>
--- src/routes/posts/[id]/+page.ts
+++ src/routes/posts/[id]/+page.ts
@@ -2,7 +2,23 @@ import { PUBLIC_API_BASE } from '$env/static/public';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch, params }) => {
-	const res = await fetch(`${PUBLIC_API_BASE}/posts/${params.id}`);
-	const data = await res.json();
-	return data;
+	// input validity check - pre api call
+	const id = Number(params.id);
+	if (isNaN(id)) {
+		throw new Error('A valid integer is expected');
+	}
+	let err = 'API Call Failed'
+	try {
+		const res = await fetch(`${PUBLIC_API_BASE}/posts/${params.id}`);
+		const data = await res.json();
+
+		if (Object.keys(data).length > 0) {
+			return data;
+		} else {
+			err = 'Empty Object';
+			throw new Error(err)
+		}
+	} catch {
+		throw new Error(err);
+	}
 }) satisfies PageLoad;
